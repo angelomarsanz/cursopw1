713595b027573d6d139837a9faae614be9455eb6
angelomarsanz <angelomarsanz@gmail.com>
2015-05-25 06:50:09 -0430

    Update insertar_usuario2.php

--- insertar_usuario2.php
+++ insertar_usuario2.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-	<html lang="es">
-		<head>
-			<meta charset="utf-8">
-			<meta name="description" content="Ejercicio 1 de Programación web">
-			<meta content="width=device-width, initial-scale=1, minimum-scale=1" name="viewport">
-			<title>Ejercicio 1 de programación web</title>
-			<link href="escritorio.css" rel="stylesheet">	
-		</head>	
+<!-- Abrimos la etiqueta PHP para ajustar a utf-8 y hacer un include 
+	del encabezado -->
+<?php 
+header ('Content-type: text/html; charset=utf-8'); 
+include "cabecera.php";
+?>
+<!-- Volvemos a HTML -->
+
 		<body>
 			<section class="formulario">
 
