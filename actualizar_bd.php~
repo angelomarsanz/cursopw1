11109d2b329c70aa222adccc559d5ce11238f7e4
angelomarsanz <angelomarsanz@gmail.com>
2015-05-30 09:35:11 -0430

    Versión 30-05-2015 - 1

---
+++ actualizar_bd.php~
@@ -0,0 +1,16 @@
+<?php
+
+	/* Si la variable "modificar" existe y tiene algun valor, 
+		quiere decir que el usuario introdujo datos en el formulario,
+		entonces procedemos a... */
+
+	if (isset($_REQUEST['modificar']))
+		{
+			// Transferimos los datos de las variables de HTML a las variables de PHP
+
+			$insertar = $_REQUEST['modificar'];	
+			$usuario = $_REQUEST['usuario'];
+			echo "$usuario";
+		}
+?>
+
--- respaldo modificar_usuario.php
+++ respaldo modificar_usuario.php
@@ -1,10 +1,60 @@
-<form action="modificar_usuario.php" name="modificar" method="post">
+<?php
 
-	<h1>Modificar datos:</h1>
+	/* Crear las variables necesarias para conectar con la base de datos y
+		asignarle los valores correspondientes */
 
-	<p class="etiqueta">Usuario:*</p>
-				<p class="campo"><input type="text" value="<?php echo $row['usuario']; ?>" name="usuario" size="10" maxlenght="10" readonly></p> 
-			
+	$servername = "localhost";
+	$username = "cursophp";
+	$password = "";
+	$dbname = "clientes";
+
+	/* Creamos la variable "$conn" y le asignamos la instrucción "mysqli_connect" y las variables
+		necesarias para conectar a la base de datos "clientes" */
+
+	$conn = mysqli_connect ($servername, $username, $password, $dbname);
+
+	/* Intentamos conectarnos, si al intentar la variable "$conn" retorna el valor "false", 
+		quiere decir que la conexión a la base de datos falló, entonces,
+		abortamos el programa y mostramos un mensaje de error 
+		por pantalla */
+
+	If (!($conn))
+		{
+		die ("Conexión fallida: " . mysqli_connect_error());				
+		}
+
+	/* Preparamos la variable "$sql" con las instrucciones necesarias
+		y los datos introducidos por el usuario
+		para acceder a la cuenta del usuario
+		en la base de datos "clientes" */
+				
+	$sql = "SELECT * FROM usuario WHERE usuario = '$usuario' and clave = '$clave'";
+
+	/* Intentamos acceder a la cuenta del usuario,
+		si se logra, se sigue el curso normal del programa,
+		pero si hay algún error abortamos el programa
+		y enviamos un mensaje por pantalla */
+
+  	$result = mysqli_query($conn, $sql) or die ("Fallo al acceder a la cuenta del usuario");
+
+	/* mysqli_num_rows es una función de Php que devuelve el número
+		de registros encontrados en la tabla y si es mayor a "cero"
+		quiere decir que si existe uno o más registros que coinciden
+		con el "usuario" y la "clave" */
+	
+	if (mysqli_num_rows($result) > 0) 
+		{
+		/* mysqli_fetch_asoc es una función que devuelve
+			un arreglo o array que contiene el registro encontrado
+			en la tabla. Ese arreglo lo guardamos en la variable
+			$row */
+
+		$row = mysqli_fetch_assoc($result);
+		print ("<h1>Modificar datos:</h1>");
+		print ("<p class='campo'>Usuario: " . $row['usuario'] . "</p>");
+?>
+			<form action="modificar_usuario.php" name="modificar" method="post">
+					
 				<p class="etiqueta">Clave:*</p>				
 				<p class="campo"><input type="password" value="<?php echo $row['clave']; ?>" name="clave" id="miclave" size="10" maxlenght="10" 
 
@@ -29,7 +79,7 @@
 ?>					
 								
 				<p class="etiqueta">Correo:*</p>
-				<p class="campo"><input type="email" value="<?php echo $row['correo']; ?>" name="correo" id="micorreo"
+				<p class="campo"><input type="email" value="<?php echo $row['correo']; ?>" name="correo" id="micorreo" 
 <?php
 				
 		/* Si la variable "insertar" existe y tiene algún valor, procedemos 
@@ -54,7 +104,6 @@
 				<p class="campo">
 					<select name="tipocliente">
 						<option value="<?php echo $row['tipo_cliente']; ?>" select><?php echo $row['tipo_cliente']; ?>
-						<option value="">&nbsp
 						<option value="V">V
 						<option value="E">E
 						<option value="P">P
@@ -75,7 +124,26 @@
 				<p class="etiqueta">Teléfono:</p>
 				<p class="campo"><input type="tel" value="<?php echo $row['telefono']; ?>" name="telefono" id="mitelefono"></p>
 
-				<p class="campo"><input type="submit" name="modificar" value="Modificar"></p>
+				<p class="campo"><input type="submit" name="insertar" value="Registrarse"></p>
 			</form>
 				<p class="campo">Nota: Los datos marcados con (*) deben ser rellenados obligatoriamente.</p>
 				<p class="campo"><a href="index.php">Volver al inicio</a></p>		
+
+<?php
+
+		}
+	else
+		{
+		echo "Usuario o clave inválida";
+		print ("<p><a href='acceder_usuario.php'>Volver al formulario</a></p>\n"); 
+		}
+ 			
+	/* Liberamos el resul set para reusarlo más adelante */
+   			
+	mysqli_free_result($result);
+			
+	/* Cerramos la conexión a la base de datos */
+
+	mysqli_close($conn);
+
+?>			
