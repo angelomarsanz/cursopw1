73bf188265da72351ff45731b39885f50c5369fe
angelomarsanz <angelomarsanz@gmail.com>
2015-09-01 21:30:50 -0430

    versión 01-09-2015 - 1

--- clientes.sql
+++ clientes.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.4.3
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 22-05-2015 a las 06:11:47
--- Versión del servidor: 5.6.17
--- Versión de PHP: 5.5.12
+-- Host: localhost
+-- Generation Time: Sep 02, 2015 at 03:44 AM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,60 +17,64 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `clientes`
+-- Database: `clientes`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `eventos`
+-- Table structure for table `eventos`
 --
 
 CREATE TABLE IF NOT EXISTS `eventos` (
-  `id_evento` int(11) NOT NULL AUTO_INCREMENT,
+  `id_evento` int(11) NOT NULL,
   `usuario` varchar(10) NOT NULL,
   `tipo_evento` varchar(50) NOT NULL,
   `nombre_evento` varchar(200) NOT NULL,
   `lugar_evento` varchar(200) NOT NULL,
   `fecha_evento` date NOT NULL,
-  `hora_evento` time NOT NULL,
-  PRIMARY KEY (`id_evento`),
-  KEY `usuario` (`usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `hora_evento` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `fotos`
+-- Table structure for table `fotos`
 --
 
 CREATE TABLE IF NOT EXISTS `fotos` (
-  `id_foto` int(11) NOT NULL AUTO_INCREMENT,
+  `id_foto` int(11) NOT NULL,
   `id_evento` int(11) NOT NULL,
   `foto` varchar(150) NOT NULL,
   `impresion` tinyint(1) NOT NULL,
-  `eliminada` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id_foto`),
-  KEY `id_evento` (`id_evento`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `eliminada` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `slider`
+-- Table structure for table `slider`
 --
 
 CREATE TABLE IF NOT EXISTS `slider` (
-  `id_slider` int(11) NOT NULL AUTO_INCREMENT,
-  `id_foto` int(11) NOT NULL,
-  PRIMARY KEY (`id_slider`),
-  KEY `id_foto` (`id_foto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `id_foto_slider` int(11) NOT NULL,
+  `foto_slider` varchar(100) NOT NULL,
+  `comentario_foto` varchar(200) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` (`id_foto_slider`, `foto_slider`, `comentario_foto`) VALUES
+(1, '2015-07-26 14.38.38.jpg', 'Mis hijos 1'),
+(2, '2015-07-26 14.38.41.jpg', 'Mis hijos 2'),
+(4, '2015-07-26 14.46.43.jpg', 'Mis hijos 4');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuario`
+-- Table structure for table `usuario`
 --
 
 CREATE TABLE IF NOT EXISTS `usuario` (
@@ -82,31 +86,73 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `nombres` varchar(100) NOT NULL,
   `apellidos` varchar(100) NOT NULL,
   `direccion` varchar(200) NOT NULL,
-  `telefono` varchar(15) NOT NULL,
-  PRIMARY KEY (`usuario`)
+  `telefono` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Restricciones para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Filtros para la tabla `eventos`
+-- Indexes for table `eventos`
+--
+ALTER TABLE `eventos`
+  ADD PRIMARY KEY (`id_evento`),
+  ADD KEY `usuario` (`usuario`);
+
+--
+-- Indexes for table `fotos`
+--
+ALTER TABLE `fotos`
+  ADD PRIMARY KEY (`id_foto`),
+  ADD KEY `id_evento` (`id_evento`);
+
+--
+-- Indexes for table `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`id_foto_slider`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`usuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `eventos`
+--
+ALTER TABLE `eventos`
+  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `fotos`
+--
+ALTER TABLE `fotos`
+  MODIFY `id_foto` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `id_foto_slider` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `eventos`
 --
 ALTER TABLE `eventos`
   ADD CONSTRAINT `eventos_usuario` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `fotos`
+-- Constraints for table `fotos`
 --
 ALTER TABLE `fotos`
   ADD CONSTRAINT `foto_evento` FOREIGN KEY (`id_evento`) REFERENCES `eventos` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `slider`
---
-ALTER TABLE `slider`
-  ADD CONSTRAINT `slider_foto` FOREIGN KEY (`id_foto`) REFERENCES `fotos` (`id_foto`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
